bd3d57492450d7e9f1c24b802fa2310d1816b946
sevbanbuyer <sevban.bayir@d-teknoloji.com.tr>
2024-02-03 10:56:57 +0300

    -small dots and tentacles animation added

--- app/src/main/java/com/sevban/composetutorialssevbanbayir/animations/Spring.kt
+++ app/src/main/java/com/sevban/composetutorialssevbanbayir/animations/Spring.kt
@@ -1,7 +1,9 @@
 package com.sevban.composetutorialssevbanbayir.animations
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +17,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.clipRect
 
 @Composable
 fun SpringAnimationInspection() {
@@ -27,6 +32,7 @@ fun SpringAnimationInspection() {
         offsetY += 400f
         SpringBall(ballData = it, offsetY = offsetY)
     }
+
 }
 
 @Composable
@@ -38,7 +44,11 @@ fun SpringBall(
         Animatable(0f)
     }
 
+    var smallDotsVisible by remember {
+        mutableStateOf(false)
+    }
     LaunchedEffect(key1 = true) {
+
         offsetXMultiplier.animateTo(
             1f,
             animationSpec = spring(
@@ -46,15 +56,77 @@ fun SpringBall(
                 stiffness = ballData.stiffness
             )
         )
+
+        smallDotsVisible = !offsetXMultiplier.isRunning
+
     }
 
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-
+        BeginCircle()
         Canvas(modifier = Modifier.fillMaxSize()) {
             drawCircle(
                 color = Color(0xFF36A368),
                 radius = 100f,
                 center = Offset(950f * offsetXMultiplier.value, offsetY),
+            )
+        }
+        AnimatedVisibility(visible = smallDotsVisible) {
+            SmallDot(offsetY = offsetY)
+            PathFromBeginToDots(offsetY)
+        }
+    }
+}
+
+@Composable
+fun SmallDot(offsetY: Float) {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        drawCircle(
+            color = Color(0xFF4527A0),
+            radius = 10f,
+            center = Offset(850f, offsetY),
+        )
+    }
+}
+
+@Composable
+fun BeginCircle() {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        drawCircle(
+            color = Color(0xFF4527A0),
+            radius = 50f,
+            center = Offset(100f, 1000f),
+        )
+    }
+}
+
+@Composable
+fun PathFromBeginToDots(offsetY: Float) {
+    val path = remember {
+        Path()
+    }
+    val animateY = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        animateY.animateTo(
+            targetValue = 1f,
+            animationSpec = tween(4000)
+        )
+    }
+    path.apply {
+        moveTo(150f, 1000f)
+        if (offsetY < 1000f)
+            cubicTo(850f / 3, offsetY / 3, 850f / 2, 2 * (offsetY / 2), 850f, offsetY)
+        if (offsetY > 1000f)
+            cubicTo(850f / 3, 3 * (offsetY / 3), 850f / 2, 2 * (offsetY / 2), 850f, offsetY)
+    }
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        clipRect(right = 850f * animateY.value) {
+            drawPath(
+                path,
+                color = Color(0xFF1565C0),
+                style = Stroke(8f)
             )
         }
     }
